bf8a81faa00fa3d8f1990e145f0dbea05cd4113a
Gleb <g.komelin@gmail.com>
2023-10-16 20:32:41 +0300

    bla bla bla

--- src/course/Komelin/task3/Test.java
+++ src/course/Komelin/task3/Test.java
@@ -3,8 +3,6 @@ package course.Komelin.task3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
-
 public class Test {
 
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
